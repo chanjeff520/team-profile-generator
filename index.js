85d8dd9e0f0b8b8c5c1a1037635dd356f1eb41ef
chanjeff520 <chanjeff520@gmail.com>
2022-10-25 12:57:36 -0400

    delete some code from eployees and add needed classes and packages to index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+//Required class and packages
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const generateHTML =  require("./src/generate-html");
